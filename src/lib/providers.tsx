72df13dd895bd65ff59addc11ede4298ec8ac283
Rushikeshnimkar <rushikeshnimkar396@gmail.com>
2024-10-12 18:28:07 +0530

    update

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -1,39 +1,37 @@
 'use client'
 
 import React, { ReactNode } from 'react'
-
-
 import { createWeb3Modal } from '@web3modal/wagmi/react'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
 import { State, WagmiProvider } from 'wagmi'
 import { config, projectId } from './wagmi'
 
-
-// Setup queryClient
+// Initialize React Query Client
 const queryClient = new QueryClient()
 
-if (!projectId) throw new Error('Wallet Connect Project ID is not defined')
+if (!projectId) {
+  throw new Error('Wallet Connect Project ID is not defined')
+}
 
-// Create modal
+// Initialize Web3Modal
 createWeb3Modal({
   wagmiConfig: config,
   projectId,
   enableAnalytics: true, // Optional - defaults to your Cloud configuration
-  enableOnramp: true // Optional - false as default
+  enableOnramp: true // Optional - false by default
 })
 
-export default function AppKitProvider({
-  children,
-  initialState
-}: {
+interface AppKitProviderProps {
   children: ReactNode
   initialState?: State
-}) {
+}
+
+export default function AppKitProvider({ children, initialState }: AppKitProviderProps) {
   return (
     <WagmiProvider config={config} initialState={initialState}>
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
     </WagmiProvider>
   )
 }
